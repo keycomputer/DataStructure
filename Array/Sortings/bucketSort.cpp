b2e8956dc546abd7ac96180e80626883189d8b8f
keycomputer <keycomputereducation@gmail.com>
2022-07-13 18:04:09 +0530

    d

---
+++ Array/Sortings/bucketSort.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bucketSort(int arr[], int n)
+{
+    vector<float> b[n];
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i]; 
+        b[bi].push_back(arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+        sort(b[i].begin(), b[i].end());
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
+}
+int main()
+{
+    int arr[] = { 10, 5,7,2,9,11,45};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bucketSort(arr, n);
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
